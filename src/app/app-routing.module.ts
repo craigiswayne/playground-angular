a24fd698078d33e3a4692027d13d90e6cd3b3d06
Craig Wayne <craigiswayne@gmail.com>
2023-06-08 15:41:40 +0100

    Feature/angular material forms (#8)
    * custom form control demo
    
    * Only specific items from common module imported
    
    * Added Glossary component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GlossaryComponent } from './glossary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GlossaryComponent
+  },
+  {
+    path: 'custom-form-control-demo',
+    loadChildren: () => import('./modules/custom-form-control-demo/custom-form-control-demo.module')
+      .then(m => m.CustomFormControlDemoModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GlossaryComponent } from './glossary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlossaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
