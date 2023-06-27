5010b6f87a5b199e861008e7482dd351177eec92
Craig Wayne <craigwg@microgaming.co.uk>
2023-06-27 10:26:15 +0100

    bugfixes for whole number validators

--- src/app/shared/validators/only-whole-numbers.validator.ts
+++ src/app/shared/validators/only-whole-numbers.validator.ts
@@ -3,21 +3,21 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 export function onlyWholeNumbersValidator(): ValidatorFn {
   return (control:AbstractControl) : ValidationErrors | null => {
 
-    const value = control.value;
-
-    if (!value) {
+    if (!control.value) {
       return null;
     }
 
+    const controlStringValue = control.value.toString();
+
     const validationChecks = {
-      isNumber : !isNaN(+value),
-      noDecimalPoint : /\./.test(value) === false,
-      noComma : /,/.test(value) === false,
+      isNumber : !isNaN(+controlStringValue),
+      noDecimalPoint : /\./.test(controlStringValue) === false,
+      noComma : /,/.test(controlStringValue) === false,
       /**
        * This prevents users entering in something like '000001'
        */
-      correctFormat : (+value).toString().length === value.length,
-      onlyValidCharacters : /^\d/.test(value),
+      correctFormat : (+controlStringValue).toString().length === controlStringValue.length,
+      onlyValidCharacters : /^\d/.test(controlStringValue),
     }
 
     /**
